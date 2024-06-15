50e166b1ba31d3be325940606d7438c6fa394a21
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-06-15 17:26:11 +0300

    установил Redis, добавил кеширование в urls приложения blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
+from django.views.decorators.cache import cache_page
 from blog.views import PublicationCreateView, PublicationListView, PublicationDetailView, PublicationUpdateView, \
     PublicationDeleteView
 
@@ -8,7 +9,7 @@ app_name = BlogConfig.name
 
 urlpatterns = [
     path('create/', PublicationCreateView.as_view(), name='create'),
-    path('', PublicationListView.as_view(), name='list'),
+    path('', cache_page(60)(PublicationListView.as_view()), name='list'),
     path('view/<int:pk>', PublicationDetailView.as_view(), name='view'),
     path('edit/<int:pk>', PublicationUpdateView.as_view(), name='edit'),
     path('delete/<int:pk>', PublicationDeleteView.as_view(), name='delete'),
